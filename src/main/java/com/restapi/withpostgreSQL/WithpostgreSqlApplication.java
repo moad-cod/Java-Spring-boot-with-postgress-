2b9c7953f5def89469b15df8e221712bda3eae17
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-21 13:16:09 +0100

    Implement the application properties

---
+++ src/main/java/com/restapi/withpostgreSQL/WithpostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.withpostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithpostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithpostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/withpostgreSQL/WithpostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.withpostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithpostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
